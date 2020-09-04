174622ff62e25d36e5f00ab663e42971fc0f2bac
Jonathan <jonathanferreira.contato@gmail.com>
2020-09-03 22:28:14 -0300

    finalizando primeira parte da migracao do Express para GraphQL, Parte dos pets e clientes finalizadas.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const { GraphQLServer  } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
+const Operations = require('./infraestrutura/operations')
+const pet = require('./infraestrutura/crud/pet')
 
 
 
@@ -14,11 +16,26 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
+const Clientes = new Operations('cliente');
+const Pets = new Operations('pet')
 const resolvers = {
   Query: {
-    status: () => "servidor rodando!"
+    status: () => "servidor rodando!",
+    clientes: () => Clientes.lista(),
+    cliente: (root, {id}) => Clientes.buscaPorId(id),
+    pets: () => Pets.lista(),
+    pet: (root, { id }) => Pets.buscaPorId(id)
+  },
+  Mutation: {
+    adicionarCliente: (root, params) => Clientes.adiciona(params),
+    atualizaCliente: (root, params) => Clientes.atualiza(params), 
+    deletaCliente: (root, {id}) => Clientes.deleta(id),
+    adicionarPet: (root, params) => Pets.adiciona(params),
+    atualizaPet: (root, params) => Pets.atualiza(params),
+    deletaPet: (root, {id}) => Pets.deleta(id)
   }
 }
+
 
 const server = new GraphQLServer({
   resolvers,
