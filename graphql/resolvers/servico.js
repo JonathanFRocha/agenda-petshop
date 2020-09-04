c7590432e51bc4b3731c466e57ad757056ca0646
Jonathan <jonathanferreira.contato@gmail.com>
2020-09-04 13:57:12 -0300

    finalizando a migracao de da Api Rest para GraphQl, adicionado servicos e atendimentos em GraphQL

--- graphql/resolvers/servico.js
+++ graphql/resolvers/servico.js
@@ -3,7 +3,13 @@ const Servico = new Operations('servico');
 
 const resolvers = {
   Mutation: {
-    adicionarServico:(root, params) => Servico.adiciona(params)
+    adicionarServico:(root, params) => Servico.adiciona(params),
+    atualizaServico:(root, params) => Servico.atualiza(params),
+    deletaServico:(root, {id}) => Servico.deleta(id)
+  },
+  Query: {
+    servicos: () => Servico.lista(),
+    servico: (root, {id}) => Servico.lista(id)
   }
 
 }
--- infraestrutura/crud/servico.js
+++ infraestrutura/crud/servico.js
@@ -1,16 +1,16 @@
 const executaQuery = require('../database/queries')
 
 class Servico {
-  lista(res) {
+  lista() {
     const sql = 'SELECT * FROM Servicos'
 
-    executaQuery(res, sql)
+    return executaQuery(sql)
   }
 
-  buscaPorId(res, id) {
+  buscaPorId(id) {
     const sql = `SELECT * FROM Servicos WHERE id=${parseInt(id)}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(servicos => servicos[0])
   }
 
   adiciona(item) {
@@ -25,17 +25,17 @@ class Servico {
     }))
   }
 
-  atualiza(res, novoItem, id) {
-    const { nome, preco, descricao } = novoItem
+  atualiza(novoItem) {
+    const { id, nome, preco, descricao } = novoItem
     const sql = `UPDATE Servicos SET nome='${nome}', Preco=${preco}, Descricao='${descricao}' WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(() => novoItem)
   }
 
-  deleta(res, id) {
+  deleta(id) {
     const sql = `DELETE FROM Servicos WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(() => id)
   }
 }
 
